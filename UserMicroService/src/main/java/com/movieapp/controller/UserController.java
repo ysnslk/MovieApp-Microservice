440b52625721ac03a6c2356fdfe2e7b91f67a09f
mustafatatlikatik <mustafatatlikatik26@gmail.com>
2023-12-16 20:02:47 +0300

    -user update added

--- UserMicroService/src/main/java/com/movieapp/controller/UserController.java
+++ UserMicroService/src/main/java/com/movieapp/controller/UserController.java
@@ -2,6 +2,7 @@ package com.movieapp.controller;
 
 
 import com.movieapp.dto.request.CreateUserRequestDto;
+import com.movieapp.dto.request.UpdateUserRequestDto;
 import com.movieapp.exception.enums.FriendlyMessageCodes;
 import com.movieapp.exception.utils.FriendlyMessageUtils;
 import com.movieapp.repository.entity.User;
@@ -12,8 +13,8 @@ import com.movieapp.response.UserResponse;
 import com.movieapp.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static com.movieapp.constants.ApiUrl.*;
@@ -21,6 +22,7 @@ import static com.movieapp.constants.ApiUrl.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(USER)
+@Slf4j
 public class UserController {
     private final UserService userService;
 
@@ -30,8 +32,8 @@ public class UserController {
     @CrossOrigin("*")
     public InternalApiResponse<UserResponse> register(@PathVariable("language")ELanguage language,
                                                       @RequestBody CreateUserRequestDto createUserRequestDto){
-        User user = userService.saveUser(createUserRequestDto);
-
+        User user = userService.saveUser(language, createUserRequestDto);
+        UserResponse userResponse = convertUserResponse(user);
         return InternalApiResponse.<UserResponse>builder()
                 .friendlyMessage(FriendlyMessage.builder()
                         .title(FriendlyMessageUtils.getFriendlyMessage(language, FriendlyMessageCodes.SUCCESS))
@@ -39,7 +41,41 @@ public class UserController {
                         .build())
                 .httpStatus(HttpStatus.CREATED)
                 .hasError(false)
+                .payload(userResponse)
                 .build();
 
+    }
+
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{language}" + UPDATE +"/{userId}")
+    @Operation(summary = "This endpoint updates user", description = "Update user")
+    @CrossOrigin("*")
+    public InternalApiResponse<UserResponse> userUpdate(@PathVariable("language")ELanguage language,
+                                                        @PathVariable("userId") String userId,
+                                                        @RequestBody UpdateUserRequestDto updateUserRequestDto){
+        log.debug("[{}][updateUser] -> request: {} {}", this.getClass().getSimpleName(), userId, updateUserRequestDto);
+        User user = userService.updateUser(language, userId, updateUserRequestDto);
+        UserResponse userResponse = convertUserResponse(user);
+        log.debug("[{}][updateProduct] -> response: {}", this.getClass().getSimpleName(), userResponse);
+        return InternalApiResponse.<UserResponse>builder()
+                .friendlyMessage(FriendlyMessage.builder()
+                        .title(FriendlyMessageUtils.getFriendlyMessage(language, FriendlyMessageCodes.SUCCESS))
+                        .description(FriendlyMessageUtils.getFriendlyMessage(language, FriendlyMessageCodes.USER_SUCCESSFULLY_UPDATED))
+                        .build())
+                .httpStatus(HttpStatus.OK)
+                .hasError(false)
+                .payload(userResponse)
+                .build();
+
+    }
+
+    private UserResponse convertUserResponse(User user) {
+        return UserResponse.builder()
+                .name(user.getName())
+                .surname(user.getSurname())
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .build();
     }
 }
--- UserMicroService/src/main/java/com/movieapp/mapper/IUserMapper.java
+++ UserMicroService/src/main/java/com/movieapp/mapper/IUserMapper.java
@@ -1,7 +1,9 @@
 package com.movieapp.mapper;
 
 import com.movieapp.dto.request.CreateUserRequestDto;
+import com.movieapp.dto.request.UpdateUserRequestDto;
 import com.movieapp.repository.entity.User;
+import com.movieapp.repository.enums.ELanguage;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
@@ -10,5 +12,6 @@ import org.mapstruct.factory.Mappers;
 public interface IUserMapper {
     IUserMapper INSTANCE = Mappers.getMapper(IUserMapper.class);
 
-User saveToUser(final CreateUserRequestDto dto);
+User saveToUser(final ELanguage language, CreateUserRequestDto dto);
+
 }
